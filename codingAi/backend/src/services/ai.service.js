097e4341d79d613071d2876b7754ea43c440645b
Adarsh Singh <adarshsikarwar543@gmail.com>
2026-05-28 14:40:00 +0530

    feat: implement user authentication with registration, login, and email verification

---
+++ codingAi/backend/src/services/ai.service.js
@@ -0,0 +1,17 @@
+import { ChatMistralAI } from "@langchain/mistralai";
+
+const model = new ChatMistralAI({
+  model: "mistral-small-latest",
+  temperature: 0,
+});
+
+export function AiTest() {
+  model
+    .invoke("What is the capital of France?")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error("Error invoking Mistral AI:", error);
+    });
+}
